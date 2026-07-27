f1dcb8235d011df9c6f86b33db20f593c6568dce
khaled saied <khaledsaied080@gmail.com>
2026-07-28 02:45:48 +0300

    #5=> Add Index And Detatils Action in PlanController

---
+++ GYMMangmentSystem/Controllers/PlanController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using GymMangment.DAL.Repositorities.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GYMMangmentSystem.PL.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly IPlanRepository planRepository;
+        public PlanController(IPlanRepository planRepository)
+        {
+            this.planRepository = planRepository;
+        }
+
+
+        public async Task<IActionResult> Index(CancellationToken ct)
+        {
+            var plans = await planRepository.GetAllAsync(ct: ct);
+            return View(plans);
+        }
+
+        public async Task<IActionResult> Details(int id,CancellationToken ct)
+        {
+            var plan = await planRepository.GetByIdAsync(id,ct: ct);
+            if(plan == null)
+                return RedirectToAction(nameof(Index));
+            return View(plan);
+        }
+    }
+}
